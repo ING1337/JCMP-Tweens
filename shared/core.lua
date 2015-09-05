5cd8030b9b2505794705d3c1285bbb24a4ed5fd2
ING1337 <rubberduckish@gmx.de>
2015-09-05 08:05:32 +0200

    Create core.lua

---
+++ shared/core.lua
@@ -0,0 +1,66 @@
+
+class 'TweenCore'
+
+function TweenCore:__init()
+	self.tweens		= {}
+	self.counter	= 0
+	self.timer		= Timer()
+	Events:Subscribe("TriggerTween", self, self.TriggerTween)
+end
+
+function TweenCore:CreateID(args)
+	self.counter = self.counter + 1
+	return Settings.DefaultID .. self.counter
+end
+
+-- ########################################################################################################################################################
+-- ########################################################################################################################################################
+-- ########################################################################################################################################################
+
+function TweenCore:AddTween(args)
+	args.values	= args.values or {}
+	args.name	= args.name or self:CreateID()
+	args.time	= args.time or 1000
+	args.ticks	= args.ticks and (1000 / args.ticks) or (1000 / Settings.DefaultTicks)
+	
+	args.init	= self.timer:GetMilliseconds()
+	args.motion	= Motions[args.motion] and args.motion or "Linear"
+	args.start	= {}
+	for name, value in pairs(args.values) do
+		args.start[name] = args.object[name]
+	end
+		
+	self.tweens[args.name] = args
+	Events:Fire("DelayedEvent", {event = "TriggerTween", delay = args.ticks, args = args.name})
+	return args.name
+end
+
+function TweenCore:RemoveTween(name)
+	self.tweens[name] = nil
+end
+
+-- ########################################################################################################################################################
+-- ########################################################################################################################################################
+-- ########################################################################################################################################################
+
+function TweenCore:TriggerTween(name)
+	item = self.tweens[name]
+	if (not item) then return end
+	time = self.timer:GetMilliseconds()
+	if (time >= (item.init + item.time)) then
+		for name, value in pairs(item.values) do item.object[name] = value end
+		self.tweens[name] = nil
+		if item.events and item.events[TweenEvents.End] then
+			Events:Fire(item.events[TweenEvents.End], item)
+		end
+	else
+		for name, value in pairs(item.values) do
+			item.object[name] = Motions[item.motion](item.start[name], value, (time - item.init) / item.time)
+		end
+		
+		Events:Fire("DelayedEvent", {event = "TriggerTween", delay = item.ticks, args = item.name})
+		if item.events and item.events[TweenEvents.Tick] then
+			Events:Fire(item.events[TweenEvents.Tick], item)
+		end
+	end
+end

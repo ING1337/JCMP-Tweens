9fd13e2036cab8a16d3d07d10ce90a3b9480f0b1
ING1337 <rubberduckish@gmx.de>
2015-09-07 23:56:50 +0200

    Update core.lua

--- shared/core.lua
+++ shared/core.lua
@@ -1,10 +1,9 @@
-
 class 'TweenCore'
 
 function TweenCore:__init()
-	self.tweens		= {}
+	self.tweens	= {}
 	self.counter	= 0
-	self.timer		= Timer()
+	self.timer	= Timer()
 	Events:Subscribe("TriggerTween", self, self.TriggerTween)
 end
 
@@ -20,11 +19,11 @@ end
 function TweenCore:AddTween(args)
 	args.values	= args.values or {}
 	args.name	= args.name or self:CreateID()
-	args.time	= args.time or 1000
+	args.time	= args.time or 3000
 	args.ticks	= args.ticks and (1000 / args.ticks) or (1000 / Settings.DefaultTicks)
 	
 	args.init	= self.timer:GetMilliseconds()
-	args.motion	= Motions[args.motion] and args.motion or "Linear"
+	args.motion	= args.motion or "linear"
 	args.start	= {}
 	for name, value in pairs(args.values) do
 		args.start[name] = args.object[name]
@@ -43,10 +42,16 @@ end
 -- ########################################################################################################################################################
 -- ########################################################################################################################################################
 
-function TweenCore:SampleEvent(type, item)
-	if item.events then
-		send = item.events[TweenEvents.TickEnd] or item.events[type]
-		if send then Events:Fire(send, item) end
+function TweenCore:Render(item, time)
+	for name, value in pairs(item.values) do
+		item.object[name] = Easing[item.motion]((time - item.init), item.start[name], value, item.time)
+	end
+end
+
+function TweenCore:SampleEvent(item, type, events)
+	if events then
+		if events[TweenEvents.TickEnd] then Events:Fire(events[TweenEvents.TickEnd], item) end
+		if events[type] then Events:Fire(events[type], item) end
 	end
 end
 
@@ -55,14 +60,12 @@ function TweenCore:TriggerTween(name)
 	if (not item) then return end
 	time = self.timer:GetMilliseconds()
 	if (time >= (item.init + item.time)) then
-		for name, value in pairs(item.values) do item.object[name] = value end
+		self:Render(item, math.min(time, (item.init + item.time)))
 		self.tweens[name] = nil
-		self:SampleEvent(TweenEvents.End, item)
+		self:SampleEvent(item, TweenEvents.End, item.events)
 	else
-		for name, value in pairs(item.values) do
-			item.object[name] = Motions[item.motion](item.start[name], value, (time - item.init) / item.time)
-		end
+		self:Render(item, time)
 		Events:Fire("DelayedEvent", {event = "TriggerTween", delay = item.ticks, args = item.name})
-		self:SampleEvent(TweenEvents.Tick, item)
+		self:SampleEvent(item, TweenEvents.Tick, item.events)
 	end
 end
